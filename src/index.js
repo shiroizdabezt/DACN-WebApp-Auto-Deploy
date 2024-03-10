fd63237c0eafebb5a33e73e344e59c9f7c7ad9d8
Lê Xuân Linh <113287392+pokilee10@users.noreply.github.com>
2024-03-10 16:22:42 +0700

    khỏe biết chạy

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import ShopContextProvider from './Context/ShopContext';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <ShopContextProvider>
-    <App />
+    <App/>
   </ShopContextProvider>
 );
 
