2b5f836e3c1d852d36eddf4715868f1253db6673
Lê Xuân Linh <113287392+pokilee10@users.noreply.github.com>
2024-03-10 16:03:23 +0700

    khỏe đi đái

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
-import { ShopContext } from '../../Context/ShopContext'
+//import { ShopContext } from '../../Context/ShopContext'
 
 test('renders learn react link', () => {
-  const {getTotalCartItems, getTotalCartAmount, all_product, cartItems, addToCart, removeFromCart} = useContext(ShopContext);
+  //const {getTotalCartItems, getTotalCartAmount, all_product, cartItems, addToCart, removeFromCart} = useContext(ShopContext);
   render(<App/>);
   //const linkElement = screen.getByText(/learn react/i);
   //expect(linkElement).toBeInTheDocument();
