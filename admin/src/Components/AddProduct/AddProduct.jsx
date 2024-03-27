09a270548b5a9c020e4d516af12a9ea9163d47c9
Lê Xuân Linh <113287392+pokilee10@users.noreply.github.com>
2024-03-27 23:13:48 +0700

    woaaaaaaaaaa

--- admin/src/Components/AddProduct/AddProduct.jsx
+++ admin/src/Components/AddProduct/AddProduct.jsx
@@ -9,7 +9,7 @@ const AddProduct = () => {
     const [productDetails, setProductDetails] = useState({
         name:"",
         image:"",
-        category:"woment",
+        category:"women",
         new_price:"",
         old_price:""
     })
