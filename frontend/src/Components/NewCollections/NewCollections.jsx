b1e7e195ae0f84a6651f349312dff6b58da19fd4
Shiroizdabezt <codykhoanguyen@gmail.com>
2024-06-07 00:54:30 +0700

    update

--- frontend/src/Components/NewCollections/NewCollections.jsx
+++ frontend/src/Components/NewCollections/NewCollections.jsx
@@ -9,7 +9,7 @@ const NewCollections = () => {
   const [new_collection, setNew_collection] = useState([])
 
   useEffect(() => {
-    fetch('http://tuilalinh.id.vn:4000/newcollections')
+    fetch('https://tuilalinh.id.vn:4000/newcollections')
     .then((response) => response.json())
     .then((data) => setNew_collection(data));
   },[])
